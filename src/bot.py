cad51d488c24da8a45c3ffcca904a56d23b89433
Tai Danh <tmdanh@ucsc.edu>
2021-02-22 21:42:09 -0800

    added command to display notebook working on createNotebook

--- src/bot.py
+++ src/bot.py
@@ -2,12 +2,43 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 import config
+import notebook
+import json
+import discord
 
 bot = commands.Bot(config.prefix)
+
+
+@bot.command()
+async def createNotebook(ctx, name):
+    name = Notebook(name)
+
+
+@bot.command(pass_context=True, name="notebook")
+async def displayNotebook(ctx, name):
+    channel = ctx.message.channel
+
+    with open("data.json") as f:
+        data = json.load(f)
+
+    if name in data:
+        notebook = discord.Embed(
+            title=data[name]
+            # color=discord.Color.yellow()
+        )
+        for note in data[name]:
+            notebook.add_field(name=note, value=data[name][note], inline=True)
+
+        await ctx.send_message(channel, embed=notebook)
+
+    else:
+        await ctx.send_message(channel, "{}, this notebook does not exist".format(ctx.message.author))
+
 
 @bot.event
 async def on_ready():
     print("Ready as {}".format(bot.user))
+
 
 @bot.command()
 async def hello(ctx: Context):
--- src/notebook.py
+++ src/notebook.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 class Note:
     title: str
     content: str
@@ -9,6 +10,7 @@ class Note:
         self.title = title
         self.content = content
         self.created_at = datetime.now()
+
 
 class Notebook:
     title: str
@@ -23,15 +25,22 @@ class Notebook:
     def __len__(self):
         return len(self.notes)
 
+    def __dict__(self):
+        notebook = {self.title: {}}
+        for note in self.notes:
+            notebook[self.title][note.title] = note.content
+
     """
     Add a new note to the notebook
     """
+
     def add(self, note: Note):
         self.notes.append(note)
 
     """
     Remove a note from the notebook
     """
+
     def remove(self, note: Note):
         if note in self.notes:
             self.notes.remove(note)
